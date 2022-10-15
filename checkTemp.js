d5a7c802d79388c4fdb90c99b5b20f61dd832248
SdeMeres <115346351+SdeMeres@users.noreply.github.com>
2022-10-15 23:29:59 +0300

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -1,5 +1,5 @@
 function checkTemp(q) {
-    if (q <= 20 && q >= 0) {console.log(-1)}
-    else if (q > 40 && q <= 60) {console.log(1)}
-    else if (q <= 60 && q >= 0) {console.log(0)}
+    if (q <= 20 && q >= 0) { return -1 }
+    else if (q > 40 && q <= 60) { return 1 }
+    else if (q <= 60 && q >= 0) { return 0 }
 }
